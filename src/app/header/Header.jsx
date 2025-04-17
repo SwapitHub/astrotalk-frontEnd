c26b3f2d1639aabf84bf4628b43a118943fe4c92
shamsher singh <shamshersingh8574@gmail.com>
2025-04-17 12:32:21 +0530

    17-04-2025

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -172,7 +172,7 @@ if(userMobile){
                   <Link href="/signup">Astrologer Registration</Link>
                 </li>
                 <li>
-                  <Link href="/admin">Admin</Link>hjdgjdgsdgsjdgs
+                  <Link href="/admin">Admin</Link>
                 </li>
               </ul>
             </nav>
