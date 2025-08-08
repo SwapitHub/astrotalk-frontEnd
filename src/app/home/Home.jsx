84052b6df0ac41bbc6fd5afd60d04947b80d01b0
swapit <swapit.testing@gmail.com>
2025-08-08 18:52:12 +0530

    08-08-2025

--- src/app/home/Home.jsx
+++ src/app/home/Home.jsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
 import Banner from "./Banner";
-import Astrology from "./astrology";
 import TopSellingSlider from "../component/TopSellingSlider";
 import NewlyLaunceSlider from "../component/NewlyLaunceSlider";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import FaqHome from "./FaqHome";
+import Astrology from "./Astrology";
 
 const Home = () => {
   return (
