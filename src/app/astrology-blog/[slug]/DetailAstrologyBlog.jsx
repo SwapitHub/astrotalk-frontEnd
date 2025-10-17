bc3eb564d58cb5ce91b16682185f0a33b2fb43d5
swapit <swapit.testing@gmail.com>
2025-10-17 18:19:58 +0530

    17-10-2025

--- src/app/astrology-blog/[slug]/DetailAstrologyBlog.jsx
+++ src/app/astrology-blog/[slug]/DetailAstrologyBlog.jsx
@@ -1,24 +1,37 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const DetailAstrologyBlog = ({ blogsDetailData }) => {
   return (
-  <div className="blogs-detail-outer">
-    <div className="container">
-      <Link href="/astrology-blog">Go Back</Link>
-      <div className="blogs-detail">
-        <div className="blog-featured-img">
-        <img src={blogsDetailData?.coverImage} alt={blogsDetailData?.name} />
-        </div>
-        <div className="name-date">
-          <p>{blogsDetailData?.title}</p>
-          {new Date(blogsDetailData?.createdAt).toLocaleString()}
-        </div>
-        <div className="blog-post-content">
-          <div dangerouslySetInnerHTML={{ __html: blogsDetailData?.content }} />
+    <div className="blogs-detail-outer">
+      <div className="container">
+        <Link href="/astrology-blog">Go Back</Link>
+        <div className="blogs-detail">
+          <div className="blog-featured-img">
+            <Image
+              width={100}
+              height={100}
+              src={
+                blogsDetailData?.coverImage
+                  ? process.env.NEXT_PUBLIC_WEBSITE_URL +
+                    blogsDetailData?.coverImage
+                  : "/user-icon-image.png"
+              }
+              alt={blogsDetailData?.name}
+            />
+          </div>
+          <div className="name-date">
+            <p>{blogsDetailData?.title}</p>
+            {new Date(blogsDetailData?.createdAt).toLocaleString()}
+          </div>
+          <div className="blog-post-content">
+            <div
+              dangerouslySetInnerHTML={{ __html: blogsDetailData?.content }}
+            />
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
